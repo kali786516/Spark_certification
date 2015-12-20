22b361295a5fb5bf16ccd7479d89f02d45525294
kali786516 <kali.tummala@gmail.com>
2015-12-20 19:38:01 +0000

    latest

--- src/main/java/com/citi/certification/SparkStreamingSocket.scala
+++ src/main/java/com/citi/certification/SparkStreamingSocket.scala
@@ -16,24 +16,27 @@ object SparkStreamingSocket {
      Logger.getLogger("org").setLevel(Level.WARN)
      Logger.getLogger("akka").setLevel(Level.WARN)
 
-     val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming_app").set("spark.hadoop.validateOutputSpecs", "false")
-     val sc=new SparkContext(conf)
-     val scc = new StreamingContext(sc,Seconds(2))
-    scc.checkpoint("C:\\Users\\kalit_000\\Desktop\\typesafe\\spark_certification\\Study Guide for the Developer Certification for Apache Spark - Working Files")
-    import org.apache.spark.streaming._
+    def createSparkStreamingContext() ={
+      val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming_app").set("spark.hadoop.validateOutputSpecs", "false")
+      val sc=new SparkContext(conf)
+      val context = new StreamingContext(sc,Seconds(2))
+      context.checkpoint("C:\\Users\\kalit_000\\Desktop\\typesafe\\spark_certification\\Study Guide for the Developer Certification for Apache Spark - Working Files")
+      import org.apache.spark.streaming._
 
+     def addLengthToState(values:Seq[Int],state:Option[Int]) ={
+        Some(values.sum +state.getOrElse(0))
+      }
 
-    def addLengthToState(values:Seq[Int],state:Option[Int]) ={
-      Some(values.sum +state.getOrElse(0))
+      val stream=context.socketTextStream("localhost",9999)
+      stream.print()
+      stream.map(x => (x,x.length)).updateStateByKey(addLengthToState).print()
+      context
     }
 
+    val ssc=StreamingContext.getOrCreate("C:\\Users\\kalit_000\\Desktop\\typesafe\\spark_certification\\Study Guide for the Developer Certification for Apache Spark - Working Files",createSparkStreamingContext)
 
-    val stream=scc.socketTextStream("localhost",9999)
-    stream.print()
 
-    stream.map(x => (x,x.length)).updateStateByKey(addLengthToState).print()
-
-    scc.start()
-    scc.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
